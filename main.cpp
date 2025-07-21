e201ab674d350f0bb79fe88a35f32c6582b87690
PegaFox <pegafox@protonmail.com>
2025-07-21 11:11:33 -0400

    do all that again, but hopefully it will work this time

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <fstream>
 #include <random>
-
-#define _USE_MATH_DEFINES
 #include "utils.hpp"
 #include "menu_utils.hpp"
 #include <glm/gtc/matrix_transform.hpp>
